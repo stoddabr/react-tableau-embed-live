3ecd0b6f53c5c20025b0c57e20ece7c7c1132b51
stoddabr <stoddabr@users.noreply.github.com>
2022-12-19 22:29:48 +0000

    deploy: 7df2a312682d91a60a6943070e3f6a5de36bd25b

--- lib/TableauEmbed/types.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export declare type TableauVizRef = React.Ref<any>;
-export * from "./ScrapedTableauTypes/Enums";
-export * from "./ScrapedTableauTypes/ExternalContract_Shared_Namespaces_Tableau";
-export * from "./ScrapedTableauTypes/VizInterfaces";
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as TableauEmbed } from "./TableauEmbed";
-export { default as useTableau } from "./TableauEmbed/UseTableau";
-export * from "./TableauEmbed/types";
